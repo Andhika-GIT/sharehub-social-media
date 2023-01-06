c4498fd5c823da7b5db19917805f9dabc2707c08
Andhika Prasetya <andhikapr081@gmail.com>
2023-01-06 17:10:49 +0700

    setup home.jsx sidebar

--- sharehub_frontend/src/container/Home.jsx
+++ sharehub_frontend/src/container/Home.jsx
@@ -1,5 +1,7 @@
 import React, { useState, useRef, useEffect } from 'react';
-import { Sidebar, UserProfile } from '../components';
+
+// components
+import { Sidebar, UserProfile, Pins } from '../components';
 
 // icons
 import { HiMenu } from 'react-icons/hi';
@@ -13,7 +15,23 @@ import logo from '../assets/logo.png';
 import { client } from '../client';
 
 const Home = () => {
-  return <div>Home</div>;
+  const [toggleSidebar, setToggleSidebar] = useState(false);
+  return (
+    <div className="flex bg-gray-50 md:flex-row flex-col h-screen transaction-height duration-75 ease-out">
+      <div className="hidden md:flex h-screen flex-initial">
+        <Sidebar />
+      </div>
+      <div className="flex md:hidden flex-row">
+        <HiMenu fontSize={40} className="cursor-pointer" onClick={() => setToggleSidebar(false)} />
+        <Link to="/">
+          <img src={logo} alt="logo" className="w-28" />
+        </Link>
+        <Link to={`user-profile/${user?._id}`}>
+          <img src={logo} alt="logo" className="w-28" />
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
