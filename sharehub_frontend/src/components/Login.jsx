c93701791f77df1854a59cba16322cd29d15b69c
Andhika Prasetya <andhikapr081@gmail.com>
2023-01-06 13:49:21 +0700

    insert client id into google auth using the env global variabel

--- sharehub_frontend/src/components/Login.jsx
+++ sharehub_frontend/src/components/Login.jsx
@@ -14,7 +14,7 @@ const Login = () => {
   const responseGoogle = (credentialResponse) => {};
 
   return (
-    <GoogleOAuthProvider clientId="">
+    <GoogleOAuthProvider clientId={import.meta.env.REACT_APP_GOOGLE_API_TOKEN}>
       <div className="flex justify-start items-center flex-col h-screen">
         <div className="relative w-full h-full">
           <video src={shareVideo} type="video/mp4" loop controls={false} muted autoPlay className="w-full h-full object-cover" />
