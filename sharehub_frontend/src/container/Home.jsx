5145b366e11880cf8fdc1381eec4b147d0e3e329
Andhika Prasetya <andhikapr081@gmail.com>
2023-01-06 17:24:32 +0700

    fetching user data in Home.jsx run the query function from data.js use the sanity client to fetch the result query set the user state based on the first data of the array result

--- sharehub_frontend/src/container/Home.jsx
+++ sharehub_frontend/src/container/Home.jsx
@@ -13,13 +13,27 @@ import logo from '../assets/logo.png';
 
 // sanity
 import { client } from '../client';
+import { userQuery } from '../utils/data';
 
 const Home = () => {
   const [toggleSidebar, setToggleSidebar] = useState(false);
+  const [user, setUser] = useState(null);
 
   // check if there's user info from localstorage, then parse the data
   // otherwise clear the localStorage
   const userInfo = localStorage.getItem('user') !== 'undefined' ? JSON.parse(localStorage.getItem('user')) : localStorage.clear();
+
+  useEffect(() => {
+    // run the query function from data.js to get the user data based on the user sub (unique token)
+    const query = userQuery(userInfo?.sub);
+
+    // use the sanity client to fetch the result query
+    client.fetch(query).then((data) => {
+      // set the user state based on the first data of the array result
+      setUser(data[0]);
+    });
+  }, []);
+
   return (
     <div className="flex bg-gray-50 md:flex-row flex-col h-screen transaction-height duration-75 ease-out">
       <div className="hidden md:flex h-screen flex-initial">
@@ -31,7 +45,7 @@ const Home = () => {
           <img src={logo} alt="logo" className="w-28" />
         </Link>
         <Link to={`user-profile/${user?._id}`}>
-          <img src={logo} alt="logo" className="w-28" />
+          <img src={user?.image} alt="logo" className="w-28" />
         </Link>
       </div>
     </div>
