c26f0d2766bb7d538fc08b7d17cb20fe50e87450
Andhika Prasetya <andhikapr081@gmail.com>
2023-01-06 16:10:10 +0700

    import components and asssets to home.jsx

--- sharehub_frontend/src/container/Home.jsx
+++ sharehub_frontend/src/container/Home.jsx
@@ -1,9 +1,17 @@
 import React, { useState, useRef, useEffect } from 'react';
+import { Sidebar, UserProfile } from '../components';
+
+// icons
 import { HiMenu } from 'react-icons/hi';
 import { AiFillCloseCircle } from 'react-icons/ai';
 import { Link, Route, Routes } from 'react-router-dom';
 
-import { Sidebar, UserProfile } from '../components';
+// assets
+import logo from '../assets/logo.png';
+
+// sanity
+import { client } from '../client';
+
 const Home = () => {
   return <div>Home</div>;
 };
