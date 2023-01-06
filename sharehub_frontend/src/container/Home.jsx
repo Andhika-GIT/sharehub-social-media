678ea680cd94cf68f2b76580746c6c555d5a8671
Andhika Prasetya <andhikapr081@gmail.com>
2023-01-06 16:08:09 +0700

    new file for importing all component instead of importing component in every single file we create a file that holds all component so we can use that file to all component without writing multiple lines for import

--- sharehub_frontend/src/container/Home.jsx
+++ sharehub_frontend/src/container/Home.jsx
@@ -3,8 +3,7 @@ import { HiMenu } from 'react-icons/hi';
 import { AiFillCloseCircle } from 'react-icons/ai';
 import { Link, Route, Routes } from 'react-router-dom';
 
-import Sidebar from '../components/Sidebar';
-import UserProfile from '../components/UserProfile';
+import { Sidebar, UserProfile } from '../components';
 const Home = () => {
   return <div>Home</div>;
 };
