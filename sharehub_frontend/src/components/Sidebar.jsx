124d828fa6aeb69be97d167453dd9759c8726bbb
Andhika Prasetya <andhikapr081@gmail.com>
2023-01-07 14:50:02 +0700

    profile image in the bottom of sidebar

--- sharehub_frontend/src/components/Sidebar.jsx
+++ sharehub_frontend/src/components/Sidebar.jsx
@@ -25,7 +25,7 @@ const Sidebar = ({ user, closeToggle }) => {
   };
 
   return (
-    <div className="fle flex-col justify-between bg-white h-full overflow-y-scroll min-w-210 hide-scrollbar">
+    <div className="flex flex-col justify-between bg-white h-full overflow-y-scroll min-w-210 hide-scrollbar">
       <div className="flex flex-col">
         <Link to="/" className="flex px-5 gap-2 my-6 pt-1 w-190 items-center" onClick={handleCloseSidebar}>
           <img src={logo} alt="logo" className="w-full" />
@@ -42,6 +42,12 @@ const Sidebar = ({ user, closeToggle }) => {
           ))}
         </div>
       </div>
+      {user && (
+        <Link to={`user-profile/${user._id}`} className="flex my-5 mb-3 gap-2 p-2 items-center bg-white rounded-lg shadow-lg mx-3" onClick={handleCloseSidebar}>
+          <img src={user.image} className="w-10 h-10 rounded-full" alt="user-profile" />
+          <p>{user.userName}</p>
+        </Link>
+      )}
     </div>
   );
 };
