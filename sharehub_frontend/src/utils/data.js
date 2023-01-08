25aad2f5d13a375d00b669269ce980f8e04fe6c1
Andhika Prasetya <andhikapr081@gmail.com>
2023-01-08 19:29:08 +0700

    sanity GROQ to get refrence data

--- sharehub_frontend/src/utils/data.js
+++ sharehub_frontend/src/utils/data.js
@@ -9,6 +9,27 @@ export const userQuery = (userId) => {
 export const searchQuery = (searchTerm) => {
   // get the posts from the user search, based on the title or category that matches the user search
   const query = `*[_type == "pin" && title match '${searchTerm}*' || category match '${searchTerm}*' || about match '${searchTerm}*']{
-    
+    image {
+      asset -> {
+        url
+      }
+    },
+    _id,
+    destination,
+    postedBy -> {
+      _id
+      userName,
+      image
+    },
+    save[] {
+      _key,
+      postedBy -> {
+        _id,
+        userName,
+        image
+      },
+    },
   }`;
+
+  return query;
 };
