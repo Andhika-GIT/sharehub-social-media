f92e7885693781522640b4f1e7c6becfc5c7b88a
Andhika Prasetya <andhikapr081@gmail.com>
2023-01-09 14:18:07 +0700

    CreatePin.jsx setup the component import all the stuffs

--- sharehub_frontend/src/components/CreatePin.jsx
+++ sharehub_frontend/src/components/CreatePin.jsx
@@ -1,6 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const CreatePin = () => {
+// icons
+import { AiOutlineCloudUpload } from 'react-icons/ai';
+import { MdDelete } from 'react-icons/md';
+
+import { useNavigate } from 'react-router-dom';
+
+// sanity
+import { client } from '../client';
+
+// react spinner
+import Spinner from './Spinner';
+
+const CreatePin = ({ user }) => {
   return <div>CreatePin</div>;
 };
 
