dfec0b8672f51a3ecfd3eda52227e470a9040b81
Andhika Prasetya <andhikapr081@gmail.com>
2023-01-09 13:54:11 +0700

    Pin.jsx creating link destination for the post if destination props available

--- sharehub_frontend/src/components/Pin.jsx
+++ sharehub_frontend/src/components/Pin.jsx
@@ -104,6 +104,14 @@ const Pin = ({ pin: { postedBy, image, _id, destination, save } }) => {
                 </button>
               )}
             </div>
+            <div className="flex justify-between items-center gap-2 w-full">
+              {destination && (
+                <a href={destination} target="_blank" rel="noreferrer" className="bg-white flex items-center gap-2 text-black font-bold p-2 pl-4 pr-4 rounded-full opacity-70 hover:100 hover:shadow-md">
+                  <BsFillArrowUpRightCircleFill />
+                  {destination.length > 20 ? destination.slice(8, 20) : destination.slice(8)}
+                </a>
+              )}
+            </div>
           </div>
         )}
       </div>
