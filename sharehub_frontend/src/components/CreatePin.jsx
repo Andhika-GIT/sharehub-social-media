e6847cf6874a658bef0601bee9c4e066a40b1b6d
Andhika Prasetya <andhikapr081@gmail.com>
2023-01-09 14:26:24 +0700

    CreatePin.jsx setup all state

--- sharehub_frontend/src/components/CreatePin.jsx
+++ sharehub_frontend/src/components/CreatePin.jsx
@@ -13,6 +13,15 @@ import { client } from '../client';
 import Spinner from './Spinner';
 
 const CreatePin = ({ user }) => {
+  const [title, setTitle] = useState('');
+  const [about, setAbout] = useState('');
+  const [destination, setDestination] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [fields, setFields] = useState(null);
+  const [category, setCategory] = useState(null);
+  const [imageAsset, setImageAsset] = useState(null);
+  const [wrongImageType, setWrongImageType] = useState(false);
+
   return <div>CreatePin</div>;
 };
 
