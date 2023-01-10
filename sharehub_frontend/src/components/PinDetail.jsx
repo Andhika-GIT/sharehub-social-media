f57f40e77842ed759e3a7db461ccc8e60a8751d3
Andhika Prasetya <andhikapr081@gmail.com>
2023-01-10 20:31:13 +0700

    PinDetail.jsx function for adding comment

--- sharehub_frontend/src/components/PinDetail.jsx
+++ sharehub_frontend/src/components/PinDetail.jsx
@@ -42,6 +42,40 @@ const PinDetail = ({ user }) => {
     }
   };
 
+  // method for handling add comment
+  const addComment = () => {
+    // if we already have a comment
+    if (comment) {
+      // set addingComment state to true
+      setAddingComment(true);
+
+      client
+        // add comment based on the pinId param
+        .patch(pinId)
+        // if we don't have any comment, then set the comment into empty array
+        .setIfMissing({ comments: [] })
+        // insert the comment
+        .insert('after', 'comments[-1]', [
+          {
+            comment,
+            _key: uuidv4(),
+            postedBy: {
+              _type: 'postedBy',
+              _ref: user._id,
+            },
+          },
+        ])
+        .commit()
+        .then(() => {
+          // run the fetchPinDetails again
+          fetchPinDetails();
+          // reset the comment
+          setComment('');
+          setAddingComment(false);
+        });
+    }
+  };
+
   useEffect(() => {
     fetchPinDetails();
   }, [pinId]);
@@ -94,6 +128,9 @@ const PinDetail = ({ user }) => {
             <img className="w-10 h-10 rounded-full cursor-pointer" src={pinDetail.postedBy?.image} alt="user-profile" />
           </Link>
           <input className="flex-1 border-gray-100 outline-none border-2 p-2 rounded-2xl focus:border-gray-300" type="text" placeholder="Add a comment" value={comment} onChange={(e) => setComment(e.target.value)} />
+          <button type="button" className="bg-red-500 text-white rounded-full px-6 py-2 font-semibold text-base outline-none" onClick={addComment}>
+            {addingComment ? 'Posting the comment...' : 'Post'}
+          </button>
         </div>
       </div>
     </div>
