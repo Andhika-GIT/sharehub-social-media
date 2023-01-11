931b0b2a739b66bf54d1838e0026693febd548c2
Andhika Prasetya <andhikapr081@gmail.com>
2023-01-11 23:06:57 +0700

    Search.jsx setup the component import all the stuff and setup state

--- sharehub_frontend/src/components/Search.jsx
+++ sharehub_frontend/src/components/Search.jsx
@@ -1,7 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-const Search = () => {
-  return <div>Search</div>;
+import MasonryLayout from './MasonryLayout';
+import { client } from '../client';
+import { feedQuery, searchQuery } from '../utils/data';
+import Spinner from './Spinner';
+
+const Search = ({ searchTerm }) => {
+  const [pins, setPins] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  return (
+    <div>
+      {loading && <Spinner message="searching for pins" />}
+      {pins?.length !== 0 && <MasonryLayout pins={pins} />}
+      {pins?.length === 0 && searchTerm !== '' && !loading && <div className="mt-10 text-center text-xl">No Pins Found!</div>}
+    </div>
+  );
 };
 
 export default Search;
