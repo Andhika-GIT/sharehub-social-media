104a2739332bc39657befb893b0970b8aa49ac06
Andhika Prasetya <andhikapr081@gmail.com>
2023-01-11 20:06:46 +0700

    UserProfile.jsx setup component imports all stuff and setup useState

--- sharehub_frontend/src/components/UserProfile.jsx
+++ sharehub_frontend/src/components/UserProfile.jsx
@@ -1,6 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { AiOutlineLogout } from 'react-icons/ai';
+import { useParams, useNavigate } from 'react-router-dom';
+import { googleLogout } from '@react-oauth/google';
+
+import { client } from '../client';
+import MasonryLayout from './MasonryLayout';
+import Spinner from './Spinner';
 
 const UserProfile = () => {
+  const [user, setUser] = useState(null);
+  const [pins, setPins] = useState(null);
+  const [text, setText] = useState('Created');
+  const [activeBtn, setActiveBtn] = useState('created');
+
+  const navigate = useNavigate();
+  const { userId } = useParams();
+
+  if (!user) {
+    return <Spinner message="loading profile..." />;
+  }
   return <div>UserProfile</div>;
 };
 
