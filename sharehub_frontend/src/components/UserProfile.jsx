4ebff89a99a39fe6ca0450a7919042c10381408d
Andhika Prasetya <andhikapr081@gmail.com>
2023-01-11 20:38:02 +0700

    UserProfile.jsx implement logout logic and logout button

--- sharehub_frontend/src/components/UserProfile.jsx
+++ sharehub_frontend/src/components/UserProfile.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { AiOutlineLogout } from 'react-icons/ai';
 import { useParams, useNavigate } from 'react-router-dom';
-import { googleLogout } from '@react-oauth/google';
+import { GoogleOAuthProvider, googleLogout } from '@react-oauth/google';
 
 import { client } from '../client';
 import MasonryLayout from './MasonryLayout';
@@ -31,21 +31,36 @@ const UserProfile = () => {
     });
   }, [userId]);
 
+  const logoutHandler = () => {
+    localStorage.clear();
+
+    navigate('/login');
+  };
+
   if (!user) {
     return <Spinner message="loading profile..." />;
   }
   return (
-    <div className="relative pb-2 h-full justify-center items-center">
-      <div className="flex flex-col pb-5">
-        <div className="relative flex flex-col mb-7">
-          <div className="flex flex-col justify-center items-center">
-            <img className=" w-full h-370 2xl:h-510 shadow-lg object-cover" src={randomImage} alt="user-pic" />
-            <img className="rounded-full w-20 h-20 -mt-10 shadow-xl object-cover" src={user?.image} alt="user-pic" />
-            <h1 className="fontold text-3xl texxt-center mt-3">{user?.userName}</h1>
+    <GoogleOAuthProvider clientId={import.meta.env.VITE_REACT_APP_GOOGLE_API_TOKEN}>
+      <div className="relative pb-2 h-full justify-center items-center">
+        <div className="flex flex-col pb-5">
+          <div className="relative flex flex-col mb-7">
+            <div className="flex flex-col justify-center items-center">
+              <img className=" w-full h-370 2xl:h-510 shadow-lg object-cover" src={randomImage} alt="user-pic" />
+              <img className="rounded-full w-20 h-20 -mt-10 shadow-xl object-cover" src={user?.image} alt="user-pic" />
+              <h1 className="fontold text-3xl texxt-center mt-3">{user?.userName}</h1>
+              <div className="absolute top-0 z-1 right-0 p-2">
+                {userId === user._id && (
+                  <button type="button" className=" bg-white p-2 rounded-full cursor-pointer outline-none shadow-md" onClick={logoutHandler}>
+                    <AiOutlineLogout color="red" fontSize={21} />
+                  </button>
+                )}
+              </div>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </GoogleOAuthProvider>
   );
 };
 
