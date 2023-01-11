88953a535c47083702f6d28fdc6b7e6dc5ff4060
Andhika Prasetya <andhikapr081@gmail.com>
2023-01-11 23:14:59 +0700

    Search.jsx useEffect for displaying pins based on what user search create element for display different kind of scenario

--- sharehub_frontend/src/components/Search.jsx
+++ sharehub_frontend/src/components/Search.jsx
@@ -9,6 +9,26 @@ const Search = ({ searchTerm }) => {
   const [pins, setPins] = useState(null);
   const [loading, setLoading] = useState(false);
 
+  useEffect(() => {
+    setLoading(true); // begin the loading
+
+    if (searchTerm) {
+      // search for spesific pins that matches the searchTerm
+      const query = searchQuery(searchTerm.toLowerCase());
+
+      client.fetch(query).then((data) => {
+        setPins(data); // insert data into pins state
+        setLoading(false); // stop loading
+      });
+    } else {
+      // if there's not searchTerm then simply fetch all the pins
+      client.fetch(feedQuery).then((data) => {
+        setPins(data); // insert data into pins state
+        setLoading(false); // stop loading
+      });
+    }
+  }, [searchTerm]);
+
   return (
     <div>
       {loading && <Spinner message="searching for pins" />}
