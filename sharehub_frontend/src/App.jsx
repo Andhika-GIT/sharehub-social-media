a97d8c7e861933f988ecc3cd12d38c2fcffeb8ce
Andhika Prasetya <andhikapr081@gmail.com>
2023-01-12 11:48:29 +0700

    App.jsx create useEffect to redirect user to login page if the user is not yet login

--- sharehub_frontend/src/App.jsx
+++ sharehub_frontend/src/App.jsx
@@ -1,8 +1,22 @@
+import { useEffect } from 'react';
 import { Routes, Route, useNavigate } from 'react-router-dom';
 import Login from './components/Login';
 import Home from './container/Home';
+import { fetchUser } from './utils/fetchUser';
 
 const App = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    // fetch user data from localstorage using fetchUser utils function
+    const user = fetchUser();
+
+    // if there's no user, it means the user is not log in
+    if (!user) {
+      // then navigate to login page
+      navigate('/login');
+    }
+  }, []);
   return (
     <Routes>
       <Route path="login" element={<Login />} />
