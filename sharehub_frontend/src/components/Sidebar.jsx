d3c0df1bcc82e8ad242fadfe8e53e7b9c4fd2bea
Andhika Prasetya <andhikapr081@gmail.com>
2023-01-12 11:22:15 +0700

    Sidebar.jsx change logo

--- sharehub_frontend/src/components/Sidebar.jsx
+++ sharehub_frontend/src/components/Sidebar.jsx
@@ -6,7 +6,8 @@ import { RiHomeFill } from 'react-icons/ri';
 import { IoIosArrowForward } from 'react-icons/io';
 
 // assets
-import logo from '../assets/logo.png';
+// import logo from '../assets/logo.png';
+import logo_sharehub from '../assets/logo_sharehub.png';
 
 // categories
 import { categories } from '../utils/data';
@@ -28,7 +29,7 @@ const Sidebar = ({ user, closeToggle }) => {
     <div className="flex flex-col justify-between bg-white h-full overflow-y-scroll min-w-210 hide-scrollbar">
       <div className="flex flex-col">
         <Link to="/" className="flex px-5 gap-2 my-6 pt-1 w-190 items-center" onClick={handleCloseSidebar}>
-          <img src={logo} alt="logo" className="w-full" />
+          <img src={logo_sharehub} alt="logo" className="w-full" />
         </Link>
         <div className="flex flex-col gap-5">
           <NavLink to="/" className={({ isActive }) => (isActive ? isActiveStyle : isNotActiveStyle)} onClick={handleCloseSidebar}>
