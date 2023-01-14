8a76cea68e8000210cf3461df63c815bb54db5cb
Andhika Prasetya <andhikapr081@gmail.com>
2023-01-14 15:39:44 +0700

    main.jsx importing scrolltotop

--- sharehub_frontend/src/main.jsx
+++ sharehub_frontend/src/main.jsx
@@ -3,15 +3,15 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 import App from './App';
-import SmoothScroll from './utils/SmoothScroll';
+import ScrollToTop from './utils/ScrollToTop';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Router>
-      <SmoothScroll>
+      <ScrollToTop>
         <App />
-      </SmoothScroll>
+      </ScrollToTop>
     </Router>
   </React.StrictMode>
 );
